6d931a5783de84f2815d2bb821306f77febbba1c
Nastassia Chaliapina <Nastassia_Chaliapina@epam.com>
2020-05-20 15:03:06 +0300

    commit

---
+++ src/test/java/factory/FactoryChromeDriverUsage.java
@@ -0,0 +1,21 @@
+package factory;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class FactoryChromeDriverUsage {
+
+    protected WebDriver driver;
+
+    @Test
+    public void testFacrotyChromeDriver() {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        driver = new ChromeDriverCreator().createWebDriver();
+        driver.navigate().to("https://4frag.ru/");
+        driver.findElement(By.xpath("//span[text() = 'Корзина']"));
+        driver.quit();
+    }
+}
